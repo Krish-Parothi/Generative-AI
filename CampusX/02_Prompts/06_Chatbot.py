3f4c2217727221f2adfc2230dfa6acb268a9411c
Krish-Parothi <krrishparothi10@gmail.com>
2025-12-22 20:47:26 +0530

    Chatbot

--- CampusX/02_Prompts/06_Chatbot.py
+++ CampusX/02_Prompts/06_Chatbot.py
@@ -13,7 +13,7 @@ model = ChatGroq(
 )
 
 while True: 
-    user_input = input("You:")
+    user_input = input("You: ")
     if user_input == 'exit':
         break
     result = model.invoke(user_input)
