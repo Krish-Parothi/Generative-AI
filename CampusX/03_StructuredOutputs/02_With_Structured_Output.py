5d574b9f4254a052e5ce7d34d2fb31abf08269da
Krish-Parothi <krrishparothi10@gmail.com>
2025-12-23 16:52:07 +0530

    Structured Output

--- CampusX/03_StructuredOutputs/02_With_Structured_Output.py
+++ CampusX/03_StructuredOutputs/02_With_Structured_Output.py
@@ -1,6 +1,6 @@
 from langchain_groq.chat_models import ChatGroq
 from dotenv import load_dotenv
-from typing import TypedDict
+from typing import TypedDict, Annotated, Optional
 import os 
 
 load_dotenv()
@@ -15,12 +15,18 @@ model = ChatGroq(
 
 #Schema
 class Review(TypedDict):
-    summary:str
-    sentiment: str
 
+
+    key_themes: Annotated[list[str], "Write down all the key themes discussed in the review in a list."]
+    summary:Annotated[str, "A Brief Summaary of the Review"]
+    sentiment: Annotated[str,"Return Sentiment of the Review whether Positive, Negative or Neutral"]
+    pros: Annotated[Optional[list[str]],"Write Down all the pros inside the list."]
+    cons: Annotated[Optional[list[str]],"Write Down all the cons inside the list."]
 
 structured_model = model.with_structured_output(Review)
 
 result = structured_model.invoke('''I Brought that product at home but cpu was working fine but gpu was working very bad i think i have done mistake buying this product.''')
 
 print(result)
+print(result['summary'])
+print(result['sentiment'])
