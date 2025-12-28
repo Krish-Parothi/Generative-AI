70f2545dfa0396a9c02c56689cf7b5b18dbe41c4
Krish-Parothi <krrishparothi10@gmail.com>
2025-12-28 21:28:01 +0530

    Simple Chain and get graph function

---
+++ CampusX/05_Chains/02_SimpleChains.py
@@ -0,0 +1,37 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+import os
+
+
+
+load_dotenv()
+
+model = ChatGroq(
+    model="openai/gpt-oss-120b",
+    api_key=os.getenv("GROQ_API_KEY"),
+    temperature=0,
+
+)
+
+prompt = PromptTemplate(
+    template="Generate 5 Interesting Facts About {topic}",
+    input_variables=['topic']
+)
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+
+result = ""
+
+for chunk in chain.stream({"topic": "Indian Food"}):
+    for ch in chunk:
+        result += ch
+        print(ch, end="", flush=True)
+
+
+chain.get_graph().print_ascii()
+# You can visualize your Chain Procedure here, By using this function.
